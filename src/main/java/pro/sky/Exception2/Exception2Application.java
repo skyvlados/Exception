f04a5aa761375eb1600c4a708628e20156dceada
skyvlados <vladislav.repin@skyeng.ru>
2021-12-01 21:13:58 +0300

    Initial commit

---
+++ src/main/java/pro/sky/Exception2/Exception2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Exception2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exception2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exception2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Exception2/Exception2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Exception2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exception2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
